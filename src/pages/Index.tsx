621cc3ab6b2d55dcd18c85154de417b1f4ed8dcd
[dyad] <manjarrez.84@gmail.com>
2026-02-25 17:37:04 -0600

    Reverted all changes back to version e00d955c32b54a5a660752287f590dd0f120c142

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,6 @@ import AssemblySteps from "@/components/AssemblySteps";
 import BestPlants from "@/components/BestPlants";
 import KitDetails from "@/components/KitDetails";
 import TechnicalSpecs from "@/components/TechnicalSpecs";
-import NutrientCalculator from "@/components/NutrientCalculator";
 import MaintenanceTips from "@/components/MaintenanceTips";
 import Testimonials from "@/components/Testimonials";
 import FAQ from "@/components/FAQ";
@@ -29,7 +28,6 @@ const Index = () => {
         <BestPlants />
         <KitDetails />
         <TechnicalSpecs />
-        <NutrientCalculator />
         <MaintenanceTips />
         <Testimonials />
         <FAQ />
